ba74d52036576ac400319c5ad817855d371e4361
madajayasree <93071345+madajayasree@users.noreply.github.com>
2021-11-07 22:14:40 +0530

    Add files via upload

---
+++ IPL_Application_SortByTeamName/IPL_Application/Player.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPL_Application
+{
+    class Player : Team 
+
+    {
+        int id;
+        string name;
+        int age;
+        string battingStyle;
+        string bowlingStyle;
+
+        public Player()
+        {
+        }
+
+       /* public Player( int id, string name, int age, string battingStyle, string bowlingStyle)
+        {
+            
+
+            Id = id;
+            Name = name;
+            Age = age;
+            BattingStyle = battingStyle;
+            BowlingStyle = bowlingStyle;
+        }*/
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public string BattingStyle { get => battingStyle; set => battingStyle = value; }
+        public string BowlingStyle { get => bowlingStyle; set => bowlingStyle = value; }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2} {3} {4} {5} {6}",TeamName,Country, id, name, age, battingStyle, bowlingStyle);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            Player p = (Player)obj;
+            return this.battingStyle.Equals(p.battingStyle);
+        }
+
+        public static Player createPlayer(string playerDetails)
+        {
+            string[] s = playerDetails.Split(',');
+            Player p = new Player(s[0],s[1],int.Parse(s[2]),s[3],int.Parse(s[4]),s[5],s[6]);
+            return p;
+        }
+
+           
+
+       
+
+
+        //own Constructor
+        public Player(string teamName, string country, int id, string name, int age, string battingStyle, string bowlingStyle)
+            :base( teamName, country)
+        {
+
+            Id = id;
+            Name = name;
+            Age = age;
+            BattingStyle = battingStyle;
+            BowlingStyle = bowlingStyle;
+        }
+    }
+
+    
+}
---
+++ IPL_Application_SortByTeamName/IPL_Application/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPL_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter how many playes you want to enter");
+            int player_count;
+            player_count = Convert.ToInt32(Console.ReadLine());
+
+            Player[] player_obj = new Player[player_count];
+            for(int i=0; i< player_count; i++)
+            {
+                {
+                    Console.WriteLine("Enter {0} player details separated by kamas in the format of teamname,country, id,name,age,battingstyle,bowlingstyle", i + 1);
+                    string jobDetails = Console.ReadLine();
+                    Player p = Player.createPlayer(jobDetails);
+                    player_obj[i] = p;
+                }
+
+               
+            }
+            foreach (var item in player_obj)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Sorting by TeamName in asending order");
+            playerBO pbo = new playerBO();
+            Array.Sort(player_obj, pbo);
+
+        }
+    }
+}
